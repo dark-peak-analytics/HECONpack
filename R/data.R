355fdff58b959a2efa6c414ec058d47516e39722
Robert Smith <rasmith3@sheffield.ac.uk>
2023-04-11 16:58:11 +0100

    example data and vignette in app.

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Example Dataset for calcICER
+#'
+#' An example dataset on which to run calcICER
+#'
+#' @format ## `df_res_example`
+#' A data frame with 1,000 rows and 4 columns:
+#' \describe{
+#'   \item{e_base, e_int}{QALYs for base and intervention groups}
+#'   \item{c_base, c_int}{Costs for base and intervention groups}
+#'   ...
+#' }
+"df_res_example"
